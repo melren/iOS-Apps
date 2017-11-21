86c80ab595267d121084d454b759b9c5a3053639
Melanie Ren <melren@stanford.edu>
2017-11-20 18:56:01 -0800

    Initialized app with table view prototype

---
+++ coder-swag/coder-swag/Controllers/CategoriesVC.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Melanie Ren on 11/20/17.
+//  Copyright © 2017 Melanie Ren. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+
+    @IBOutlet weak var categoryTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
