6eb35f2e03c7078feb0cd3af722efa747fdb9dc0
Melanie Ren <melren@stanford.edu>
2017-12-06 17:36:11 -0800

    basic adding channel functionality with sockets, but very buggy

---
+++ Smack/Smack/Services/SocketService.swift
@@ -0,0 +1,49 @@
+//
+//  SocketService.swift
+//  Smack
+//
+//  Created by Melanie Ren on 12/6/17.
+//  Copyright © 2017 Melanie Ren. All rights reserved.
+//
+
+import UIKit
+import SocketIO
+
+class SocketService: NSObject {
+    static let instance = SocketService()
+    let manager = SocketManager(socketURL: URL(string: BASE_URL)!)
+    //let socket: SocketIOClient
+    lazy var socket = manager.defaultSocket
+    
+    override init() {
+        //socket = manager.defaultSocket
+        super.init()
+    }
+    
+    func establishConnection() {
+        socket.connect()
+    }
+    
+    func closeConnection() {
+        socket.disconnect()
+    }
+    
+    func addChannel(channelName: String, channelDescription: String, completion: @escaping CompletionHandler) {
+        socket.emit("newChannel", channelName, channelDescription)
+        completion(true)
+    }
+    
+    func getChannel(completion: @escaping CompletionHandler) {
+        socket.on("channelCreated") { (dataArray, ack) in
+            //parse out data array (Any type) name, desc, channel id
+            guard let channelName = dataArray[0] as? String else { return }
+            guard let channelDesc = dataArray[1] as? String else { return }
+            guard let channelID = dataArray[2] as? String else { return }
+            
+            let newChannel = Channel(_id: channelID, name: channelName, description: channelName, __v: 0)
+            MessageService.instance.channels.append(newChannel)
+            completion(true)
+        }
+    }
+    
+}
