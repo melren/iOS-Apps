167470cfa61627c9f7e818557025b69b64529355
Melanie Ren <melren@stanford.edu>
2017-12-11 17:31:55 -0800

    Completed all basic functionality of chat app

--- Smack/Smack/View/MessageCell.swift
+++ Smack/Smack/View/MessageCell.swift
@@ -25,6 +25,22 @@ class MessageCell: UITableViewCell {
         usernameLbl.text = message.userName
         userImage.image = UIImage(named: message.userAvatar)
         userImage.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+    
+        // 2017-12-11T17:16:34.590Z, ISO 8601 date format
+        guard var isoDate = message.timeStamp else { return }
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = String(isoDate[..<end])
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timestampLbl.text = finalDate
+        }
     }
 
 }
