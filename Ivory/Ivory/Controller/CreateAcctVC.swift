5338bb140cf4352dd3208c93436f48bb51b7fb46
Melanie Ren <melren@stanford.edu>
2018-03-13 20:57:19 -0700

    Set up basic auth service

--- Ivory/Ivory/Controller/CreateAcctVC.swift
+++ Ivory/Ivory/Controller/CreateAcctVC.swift
@@ -11,16 +11,47 @@ import UIKit
 class CreateAcctVC: UIViewController {
 
     // Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    @IBOutlet weak var errorTxt: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     // Actions
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
+    }
+    
+    @IBAction func createAcctPressed(_ sender: Any) {
+        guard let username = usernameTxt.text , usernameTxt.text != "" else {
+            errorTxt.isHidden = false
+            return
+        }
+        guard let email = emailTxt.text , emailTxt.text != "" else {
+            errorTxt.isHidden = false
+            return
+        }
+        guard let pass = passTxt.text, passTxt.text != "" else {
+            errorTxt.isHidden = false
+            return
+        }
+        errorTxt.isHidden = true
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("registered user")
+            }
+        }
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    @IBAction func pickBGColorPressed(_ sender: Any) {
     }
     
 
--- Ivory/Ivory/Utilities/Constants.swift
+++ Ivory/Ivory/Utilities/Constants.swift
@@ -8,10 +8,34 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// Colors
+let IVORY_PLACEHOLDER = #colorLiteral(red: 0.409147501, green: 0.5773295164, blue: 0.751957953, alpha: 0.5)
+
 // Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCT = "toCreateAcct"
 let UNWIND = "unwindToChannel"
 
-// Misc
-let IVORY_PLACEHOLDER = #colorLiteral(red: 0.409147501, green: 0.5773295164, blue: 0.751957953, alpha: 1)
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+// URLS
+let BASE_URL = "https://mivory.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+// Headers
+let HEADER = [
+    "Content-Type": "application/json; charset=utf-8"
+]
+
+let BEARER_HEADER = [
+    "Authorization": "Bearer \(AuthService.instance.authToken)",
+    "Content-Type": "application/json; charset=utf-8"
+]
+
+
+
